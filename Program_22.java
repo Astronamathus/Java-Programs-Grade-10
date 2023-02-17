1cdffb4fcf71069404223ed0fe308933d1088747
Astro <82964942+Astronamathus@users.noreply.github.com>
2023-02-17 17:50:29 +0530

    Update Program_22.java

--- Program_22.java
+++ Program_22.java
@@ -7,7 +7,7 @@ class Program_22
 {
     public static void main()
     {
-        int a[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        char a[][] = {{'a', 'b', 'c'}, {'e', 'f', 'g'}, {'h', 'i', 'j'}};
         for(int i = 2; i >= 0; i--)
         {
             for(int j = 2; j >= 0; j--)
