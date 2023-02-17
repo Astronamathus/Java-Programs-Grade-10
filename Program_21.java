618bb4a79f2e4088a70934e3ea0dfdd9b8ba6bec
Astro <82964942+Astronamathus@users.noreply.github.com>
2023-02-17 17:14:02 +0530

    Add files via upload

---
+++ Program_21.java
@@ -0,0 +1,21 @@
+//Program_21
+/*Program to initialise 9 numbers into a 2D array and print it in a matrix*/
+//13.2.23
+//Suday Dutta
+//Greenwood High
+import java.util.Scanner;
+class Program_21
+{
+    public static void main()
+    {
+        int a[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
